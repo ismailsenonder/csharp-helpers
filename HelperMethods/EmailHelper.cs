fd88e07639fd3cd9f772cb33a150692f11e00b6c
İsmail Şenönder <i.senonder@gmail.com>
2017-12-23 22:54:55 +0200

    Added testapp

--- HelperMethods/EmailHelper.cs
+++ HelperMethods/EmailHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
@@ -53,6 +54,63 @@ namespace HelperMethods
                 return Tuple.Create(false, "Fail: " + ex.ToString());
             }
         }
+        #endregion
+
+        #region SendMailWithGmailAddress
+        /// <summary>
+        /// Sends Email - First method caused error when trying to send through Gmail. So this is the second one :)
+        /// </summary>
+        /// <param name="smtpServer">smtp server ip or address as string: "mail.senonder.com"</param>
+        /// <param name="to">to e-mail address as string.</param>
+        /// <param name="toName">to e-mail name as string.</param>
+        /// <param name="from">from e-mail address as string (also counts as smtp user name)</param>
+        /// <param name="fromName">e-mail from name as string</param>
+        /// <param name="portNumber">port number as int. E.g. 25 or 587</param>
+        /// <param name="mailsubject">e-mail subject as string</param>
+        /// <param name="mailbody">e-mail HTML body (or plain text) as string</param>
+        /// <param name="password">smtp password as string</param>
+        /// <param name="enableSsl">bool</param>
+        /// <returns>Tuple containing a bool and a string</returns>
+        /// bool: true if success, false if fail
+        /// string: "Success" if success, "Fail: " + exception message if fail
+        public Tuple<bool, string> SendMailWithGmailAddress(string smtpServer, string to, string toName, string from, string fromName, int portNumber,
+            string mailsubject, string mailbody, string password, bool enableSsl)
+        {
+            try
+            {
+                var fromAddress = new MailAddress(from, fromName);
+                var toAddress = new MailAddress(to, toName);
+                string fromPassword = password;
+                string subject = mailsubject;
+                string body = mailbody;
+
+                var smtp = new SmtpClient
+                {
+                    Host = smtpServer,
+                    Port = portNumber,
+                    EnableSsl = enableSsl,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+                };
+                using (var message = new MailMessage(fromAddress, toAddress)
+                {
+                    Subject = subject,
+                    Body = body
+                })
+                {
+                    smtp.Send(message);
+                }
+
+                return Tuple.Create(true, "Success");
+
+            }
+            catch (Exception ex)
+            {
+                return Tuple.Create(false, "Fail: " + ex.ToString());
+            }
+        }
+
         #endregion
     }
 }
---
+++ TestApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using HelperMethods;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            EmailHelper mailHelper = new EmailHelper();
+            
+        }
+    }
+}

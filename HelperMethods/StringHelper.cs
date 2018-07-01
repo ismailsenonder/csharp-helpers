c5645c99e5624187215ccbb827ec6104ac508327
İsmail Şenönder <i.senonder@gmail.com>
2018-07-01 23:13:08 +0300

    added new methods

--- HelperMethods/StringHelper.cs
+++ HelperMethods/StringHelper.cs
@@ -166,5 +166,28 @@ namespace HelperMethods
             return returnValue;
         }
         #endregion
+
+        #region Base64Encode
+        public string Base64Encode(string plainText)
+        {
+            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            return Convert.ToBase64String(plainTextBytes);
+        }
+
+        #endregion 
+
+        #region CreatePassword
+        public string CreatePassword(int length)
+        {
+            const string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            StringBuilder res = new StringBuilder();
+            Random rnd = new Random();
+            while (0 < length--)
+            {
+                res.Append(valid[rnd.Next(valid.Length)]);
+            }
+            return res.ToString();
+        }
+        #endregion
     }
 }
